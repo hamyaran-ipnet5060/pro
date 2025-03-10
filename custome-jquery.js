16a40a0c0ec65d0940a0201a415fa8a46dfe5958
hamyaran-vpns3 <hamyaranet@gmail.com>
2025-03-10 19:46:07 +0330

    Add files via upload

--- custome-jquery.js
+++ custome-jquery.js
@@ -1,14 +1,6 @@
   const buttons = document.querySelectorAll('.verify-btn');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-			//window.location.href = "https://hamyaran-vps.shop";
-            window.location.href = "#Subscription-fee";
-        });
-    });
-
-   document.querySelectorAll(".subscription-option").forEach(option => {
-        option.addEventListener("click", function() {
-            var link = this.getAttribute("data-link");
-            window.location.href = link;
+            window.location.href = "https://hamyaran-vps.shop";
         });
     });
